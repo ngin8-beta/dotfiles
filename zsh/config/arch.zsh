1312f4db366ceb9494b69697a376d9cd6f22be57
v8yte <77710475+v8yte@users.noreply.github.com>
2024-06-19 23:13:47 +0900

    fix: picom auto start

--- zsh/config/arch.zsh
+++ zsh/config/arch.zsh
@@ -8,3 +8,8 @@ alias yayupdate='yay -Syu && paccache -r && paccache -ruk3'
 # 背景設定
 background="/usr/share/endeavouros/backgrounds/girl.jpeg"
 feh --no-fehbg --bg-fill $background
+
+if ! pgrep -x "picom" > /dev/null
+then
+    picom -b
+fi
